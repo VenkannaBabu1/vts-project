49d0150d599850cbf8395f9287e11dfc7f2a877a
K L R V Mallesh <lalithkundem143@gmail.com>
2024-07-05 17:29:48 +0530

    updated

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -240,7 +240,7 @@ const Register = () => {
               label="Pincode"
               rules={[
                 { required: true, message: 'Please input your pincode!' },
-                { len: 6, message: 'Pincode must be 6 digits long!' },
+                { pattern: /^[0-9]{6}$/, message: 'Pincode must be 6 digits long!' },
               ]}
               className='form-item'
             >
@@ -267,7 +267,7 @@ const Register = () => {
               label="Phone Number"
               rules={[
                 { required: true, message: 'Please input your phone number!' },
-                { pattern: /^[0-9]+$/, len: 10, message: 'Phone number must contain 10 digits and must not contain any characters' },
+                { pattern: /^[0-9]{10}$/, message: 'Phone number must contain 10 digits and must not contain any characters' },
               ]}
               className='form-item'
             >
