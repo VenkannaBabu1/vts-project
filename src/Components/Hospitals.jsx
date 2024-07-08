9486e8ab2cea8154f550b95ffb03baaaa78d2ab1
BONAM HARI VENU NARASIMHA SIDDHU <hvnsiddhu7@gmail.com>
2024-07-08 21:11:30 +0530

    css

---
+++ src/Components/Hospitals.jsx
@@ -0,0 +1,187 @@
+import './Packages.css';
+import Footer from "./Footer";
+import Navbar from './Navbar/Navbar';
+
+const Hospitals = () => {
+
+
+
+  return (
+    <div className='h-screen'>
+            <Navbar />
+    <div>
+      <div className="PACKAGES" id="PACKAGES">
+        <h1 className="heading">Hospitals</h1>
+        <div className="box-container">
+
+
+    
+            
+
+
+          <div className="box-pack">
+            <img src="https://w0.peakpx.com/wallpaper/581/14/HD-wallpaper-gateway-of-india-cst-mumbai.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location:Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <h3>
+              <li>Name:Apollo Hospitals</li>
+              </h3>
+              <div className="price">
+               <p>Contact :<span> 845456789</span></p>
+              </div>
+              {/* <a href="/gallery" className="btn">Book Now</a> */}
+            </div>
+          </div>
+    
+
+      
+          <div className="box-pack">
+            <img src="https://w0.peakpx.com/wallpaper/702/593/HD-wallpaper-victoria-memorial-kolkata-india-2022-bing.jpg" alt="" />
+            <div className="content-p">
+              <h3 >
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai,kolkata
+              </h3>
+              <h3 >
+              <li>Name: Acharya Multi Speaciality Hospitals</li>
+              </h3>
+              
+              <div className="price">
+               <p>Contact :<span> 456789125</span></p>
+              </div>
+              {/* <a href="/gallery" className="btn">Book Now</a> */}
+            </div>
+          </div>
+     
+
+     
+          <div className="box-pack">
+            <img src="https://i.pinimg.com/originals/0a/21/ca/0a21ca209a71f1c0404da30411be770f.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <h3 >
+              <li>Name : Apple Multi Speaciality Hospitals</li>
+              </h3>
+              <div className="price">
+               <p>contact :<span> 4578123954</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+
+
+          <div className="box-pack">
+            <img src="https://www.pixel4k.com/wp-content/uploads/2018/09/eiffel-tower-paris-4k_1538069087.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              
+              <li>Name: kaspisetti Home care Hospitals</li>
+              
+              <div className="price">
+               <p>Contact :<span> 7894561223</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+ 
+
+  
+          <div className="box-pack">
+            <img src="https://c4.wallpaperflare.com/wallpaper/974/957/304/abisko-4k-5k-sweden-wallpaper-preview.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <li>Name: Siddhu Multi Speaciality Hospitals</li>
+              
+              <div className="price">
+               <p>Contact :<span>7894561232</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+          <div className="box-pack">
+            <img src="https://images5.alphacoders.com/653/thumb-1920-653422.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                LOcation : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <li>Name : Harika Multi Speciality Hospitals</li>
+              
+              <div className="price">
+               <p>Contact : <span> 78978723</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+
+          <div className="box-pack">
+            <img src="https://wallpapers.com/images/hd/4k-anime-tokyo-b7z122rhgd2y9n5f.jpg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                LOcation : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <li>Name : Satyavathi Multi Speciality Hospitals</li>
+              
+              <div className="price">
+               <p>Contact : <span> 9959509439</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+
+          <div className="box-pack">
+            <img src="https://hips.hearstapps.com/hmg-prod/images/long-boat-docked-on-beach-in-krabi-thailand-summers-royalty-free-image-1622044679.jpg?crop=1xw:1xh;center,top&resize=980:*" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <li>Name : Kanaka Raju Multi Speciality Hospitals</li>
+              
+              <div className="price">
+               <p>Contact :<span>987456512</span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+
+          <div className="box-pack">
+            <img src="https://www.jetsetter.com//uploads/sites/7/2018/04/9s9_ATCO-1380x921.jpeg" alt="" />
+            <div className="content-p">
+              <h3>
+                
+                Location : Mumbai,Vizag,Goa,Amaravathi,Chennai
+              </h3>
+              <li>Name : Hari mUlti Speciality Hospitals </li>
+              
+              <div className="price">
+               <p>Contact :<span> 789456123 </span></p>
+              </div>
+              {/* <a href="/login" className="btn">Book Now</a> */}
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <Footer />
+    </div>
+    
+  );
+}
+
+export default Hospitals;
