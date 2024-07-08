61d2afad6d288c41fb0ccdc9d3d48c994f38f686
BONAM HARI VENU NARASIMHA SIDDHU <hvnsiddhu7@gmail.com>
2024-07-08 12:58:58 +0530

    Update ViewAllPolicies.jsx

--- src/Components/ViewAllPolicies.jsx
+++ src/Components/ViewAllPolicies.jsx
@@ -103,12 +103,12 @@ function AllPolicies() {
                   </div>
                 
                   <button
-                    className="policy-button w-[230px] ml-1 mt-1 hover:bg-blue-500 hover:text-white rounded-xl pb:0"
+                    className="policy-button w-[230px] ml-2 mt-1 hover:bg-blue-500 hover:text-white rounded-xl pb:0"
                     onClick={() => handleClick(policy.id, policy.type, policy.policyId, policy.policyName, policy.coverage, policy.description)}
                   >
                     Edit now
                   </button>
-                  <button className="policy-button w-[230px] ml-1 mt-2  hover:text-white rounded-xl pb:1" onClick={() => handleDelete(policy.id)}> Delete</button>
+                  <button className="policy-button w-[230px] ml-2 mt-2  hover:text-white rounded-xl pb:1" onClick={() => handleDelete(policy.id)}> Delete</button>
                 </div>
                 
               </li>
